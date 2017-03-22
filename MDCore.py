bed927ca66a5cd8f70fc20c5500732763718a33e
CameronCurrie <admin@cameroncurrie.co.uk>
2017-03-22 13:55:30 +0000

    Code fully working with energy

--- MDCore.py
+++ MDCore.py
@@ -1,9 +1,13 @@
+"""
+Main code that runs the functions
+"""
+
 import MDUtilities as MDU
 import Particlecreator as creator
 import verletintegration as verlet
 import sys
 
-
+#Must run code with two arguments, the file you wish to write to and the file you are reading from
 if len(sys.argv)!=3:
 	print ("\nWrong number of arguments.")
 	print ("Usage: " + sys.argv[0] + " <output file>" + "<input file>")
@@ -20,13 +24,14 @@ inputfile = open(inputfileName, "r")
 #reads initial condition from the file containing them
 initialconditions = verlet.conditions.readconditions(inputfile)
 
-
+#Gets initial conditions using conditons class
 PNUMBER = initialconditions.numberofatoms
 RHO = initialconditions.density
 TEMP = initialconditions.temperature
 DT = initialconditions.timeinterval
 NUMSTEP = initialconditions.numstep
 
+#names and creates the particles
 particles = creator.pNamer(PNUMBER)
 
 #set intial veloicities and positions of particles using MDUtilities
@@ -36,4 +41,5 @@ MDU.setInitialVelocities(TEMP, particles)
 #Get Boxsize for use in minimum image convention and periodic image boundary
 BOXSIZE = MDU.boxSize(RHO, PNUMBER)
 
+#run integration code which produces energy graphs and xyz file.
 verlet.verletintegration(DT, particles, BOXSIZE, NUMSTEP, outfile)
